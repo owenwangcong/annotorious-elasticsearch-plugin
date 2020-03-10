5d4bfad2d64c1d3cd2c494c5e90d09885fb59eaf
wangcongowen <owen_wangcong@hotmail.com>
2020-03-10 19:24:22 -0400

    Works with ES 7.2 now.

--- anno-elasticsearch-plugin.js
+++ anno-elasticsearch-plugin.js
@@ -9,6 +9,7 @@
 annotorious.plugin.ElasticSearch = function(opt_config_options) {
   /** @private **/
   this._STORE_URI = opt_config_options['base_url'];
+  this._INDEX_NAME = opt_config_options['index_name'];
 
   /** @private **/
   this._annotations = [];
@@ -56,7 +57,7 @@ annotorious.plugin.ElasticSearch.prototype._newLoadIndicator = function() {
  */
 annotorious.plugin.ElasticSearch.prototype._showError = function(error) {
   // TODO proper error handling
-  window.alert('ERROR');
+  window.alert('There is an error when loading annotations.');
   console.log(error);
 }
 
@@ -66,12 +67,11 @@ annotorious.plugin.ElasticSearch.prototype._showError = function(error) {
 annotorious.plugin.ElasticSearch.prototype._loadAnnotations = function(anno) {
   // TODO need to restrict search to the URL of the annotated
   var self = this;
-  jQuery.getJSON(this._STORE_URI + '_search?query=*:*&size=1000', function(data) {
+  jQuery.getJSON(this._STORE_URI + '/' + this._INDEX_NAME + '/_search?q=*:*&size=1000', function(data) {
     try {
       jQuery.each(data.hits.hits, function(idx, hit) {
         var annotation = hit['_source'];
         annotation.id = hit['_id'];
-        
         if (jQuery.inArray(annotation.id, self._annotations) < 0) {
           self._annotations.push(annotation.id);
           if (!annotation.shape && annotation.shapes[0].geometry)
@@ -94,11 +94,21 @@ annotorious.plugin.ElasticSearch.prototype._loadAnnotations = function(anno) {
  */
 annotorious.plugin.ElasticSearch.prototype._create = function(annotation) {
   var self = this;
-  jQuery.post(this._STORE_URI + 'annotation/',  JSON.stringify(annotation), function(response) {
-    // TODO error handling if response status != 201 (CREATED)
+
+  $.ajax({
+   url: this._STORE_URI + '/' + this._INDEX_NAME + '/_doc/' + Date.now() + "_" + Math.floor(Math.random()*1000),
+   type: 'PUT',
+   data: JSON.stringify(annotation),
+   dataType : "json",
+   contentType: "application/json; charset=utf-8",
+   success: function(response) {
     var id = response['_id'];
     annotation.id = id;
+	console.log('Created!');
+	console.log('annotation.id:'+annotation.id);
+   }
   });
+  
 }
 
 /**
@@ -107,10 +117,15 @@ annotorious.plugin.ElasticSearch.prototype._create = function(annotation) {
 annotorious.plugin.ElasticSearch.prototype._update = function(annotation) {
   var self = this;
   jQuery.ajax({
-    url: this._STORE_URI + 'annotation/' + annotation.id,
-    type: 'PUT',
-    data: JSON.stringify(annotation)
-  }); 
+    url: this._STORE_URI + '/' + this._INDEX_NAME + '/_doc/' + annotation.id,
+    type: 'POST',
+    data: JSON.stringify(annotation),
+    dataType : "json",
+    contentType: "application/json; charset=utf-8",
+	success: function(response) {
+		console.log('Updated!');
+    }
+ }); 
 }
 
 /**
@@ -118,7 +133,7 @@ annotorious.plugin.ElasticSearch.prototype._update = function(annotation) {
  */
 annotorious.plugin.ElasticSearch.prototype._delete = function(annotation) {
   jQuery.ajax({
-    url: this._STORE_URI + 'annotation/' + annotation.id,
+    url: this._STORE_URI + '/' + this._INDEX_NAME + '/_doc/' + annotation.id,
     type: 'DELETE'
   });
 }
